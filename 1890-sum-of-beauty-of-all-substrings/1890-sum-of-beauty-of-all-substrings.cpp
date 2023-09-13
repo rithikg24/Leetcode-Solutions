740ab199dc8e5695af9788614c2885d582d3a970
Rithik Gundeti <rithikgundeti@gmail.com>
2023-09-13 14:09:46 +0530

    Time: 305 ms (68.73%), Space: 23.7 MB (26.72%) - LeetHub

--- 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
@@ -1,22 +1,5 @@
 class Solution {
 public:
-    int calcBeauty(string s,int i,int j){
-        string sb=s.substr(i,j-i+1);
-        unordered_map<char,int> hm;
-        for(char x:sb){
-            hm[x]++;
-        }
-
-        int minFreq=INT_MAX;
-        int maxFreq=INT_MIN;
-
-        for(auto pair:hm){
-            minFreq=min(minFreq,pair.second);
-            maxFreq=max(maxFreq,pair.second);
-        }
-
-        return maxFreq-minFreq;
-    }
     int beautySum(string s) {
         int beautySum=0;
         for(int i=0;i<s.length();i++){
