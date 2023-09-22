e7318a97a8f9d7ab0d562e9383cd1c21e326f26b
Rithik Gundeti <rithikgundeti@gmail.com>
2023-09-22 12:04:17 +0530

    Time: 24 ms (5.44%), Space: 280.9 MB (5.02%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int sptr=0;
-        int tptr=0;
+        // int sptr=0;
+        // int tptr=0;
 
-        while(tptr<t.length()){
-            if(s[sptr]==t[tptr]) sptr++;
-            tptr++;
-        }
-        if(sptr==s.length()){
+        // while(tptr<t.length()){
+        //     if(s[sptr]==t[tptr]) sptr++;
+        //     tptr++;
+        // }
+        // if(sptr==s.length()){
+        //     return true;
+        // }
+        // return false;
+        return isSubsequenceFunc(s,t,0,0);
+    }
+
+    bool isSubsequenceFunc(string s, string t,int sp,int tp) {
+        if(sp==s.length()){
             return true;
         }
-        return false;
+        if(tp==t.length()){
+            return false;
+        }
+        if(s[sp]==t[tp]) return isSubsequenceFunc(s,t,sp+1,tp+1);
+        else return isSubsequenceFunc(s,t,sp,tp+1);
     }
 };
