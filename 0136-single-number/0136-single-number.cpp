26c2f1def4b5ed5f7ee239be21c210f2ae18824b
Rithik Gundeti <rithikgundeti@gmail.com>
2023-10-06 10:52:49 +0530

    Time: 16 ms (46.42%), Space: 17.2 MB (30.92%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -3,7 +3,7 @@ public:
     int singleNumber(vector<int>& nums) {
         int res=nums[0];
         for(int i=1;i<nums.size();i++){
-            res^=nums[i];
+            res=res^nums[i];
         }
         return res;
     }
