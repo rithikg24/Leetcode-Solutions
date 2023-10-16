e0409fdcf5b9ddf3cac838c231d887271a92138d
Rithik Gundeti <rithikgundeti@gmail.com>
2023-10-17 01:14:57 +0530

    Time: 126 ms (65.94%), Space: 71.6 MB (30.55%) - LeetHub

---
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int l=0;
+        int r=0;
+
+        int ans=0;
+        unordered_map<int,int> hm;
+
+        while(r<fruits.size()){
+            if(hm.count(fruits[r])){
+                hm[fruits[r]]++;
+                ans=max(ans,r-l+1);
+            }else{
+                if(hm.size()==2){
+                    while(hm.size()==2){
+                        hm[fruits[l]]--;
+                        if(hm[fruits[l]]==0){
+                            hm.erase(fruits[l]);
+                        }
+                        l++;
+                    }
+                }
+                hm[fruits[r]]=1;
+                ans=max(ans,r-l+1);
+            }
+
+
+            r++;
+        }
+        return ans;
+    }
+};
