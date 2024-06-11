d56d52761023f2acf0a6d1d2f8f6534bc00b012f
Rithik Gundeti <rithikgundeti@gmail.com>
2024-06-12 02:55:20 +0530

    Time: 24 ms (58.9%), Space: 25.7 MB (31.65%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,22 +1,18 @@
 class Solution {
-public:
-    
+public:    
     int longestCommonSubsequence(string text1, string text2) {
         vector<vector<int>> dp(text1.length()+1,vector<int>(text2.length()+1,0));
-
-        int len_a = text1.length();
-        int len_b = text2.length();
-
-        for(int i=1;i<=len_a;i++){
-            for(int j=1;j<=len_b;j++){
+       
+        for(int i=1;i<text1.length()+1;i++){
+            for(int j=1;j<text2.length()+1;j++){
                 if(text1[i-1]==text2[j-1]){
-                    dp[i][j] =1 + dp[i-1][j-1];
+                    dp[i][j]=1+dp[i-1][j-1];
                 }else{
-                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
 
-        return dp[len_a][len_b];
+        return dp[text1.length()][text2.length()];
     }
 };
