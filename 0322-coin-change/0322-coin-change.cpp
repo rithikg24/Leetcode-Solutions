d1eec4c7b888cee4b1fb0fa0afe0831175e18104
Rithik Gundeti <rithikgundeti@gmail.com>
2024-06-11 22:54:24 +0530

    Time: 35 ms (52.53%), Space: 46.7 MB (36.88%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    int helper(vector<int> coins,int i,int amt,vector<vector<int>> &dp){
-        // base case
-        if(i==0){
-            if(amt%coins[i]==0) return amt/coins[i];
-            return 1e9;
-        }
-        if(amt==0) return 0;
-
-        if(dp[i][amt]!=-1) return dp[i][amt];
-
-        // recursive case
-        int notTake = helper(coins,i-1,amt,dp); // done taking
-        int take=1e9;
-        if(coins[i]<=amt) take = 1+helper(coins,i,amt-coins[i],dp);
-        return dp[i][amt] =  min(take,notTake);
-    }
     int coinChange(vector<int>& coins, int amount) {
         vector<vector<int>> dp(coins.size(),vector<int>(amount+1,-1));
-        int res=helper(coins,coins.size()-1,amount,dp);
+
+        for(int amt=0;amt<=amount;amt++){
+            if(amt%coins[0]==0) dp[0][amt]=amt/coins[0];
+            else{
+                dp[0][amt]=1e9;
+            }
+        }
+
+        for(int i=1;i<coins.size();i++){
+            for(int amt=0;amt<amount+1;amt++){
+                int notTake = dp[i-1][amt];
+                int take=1e9;
+                if(coins[i]<=amt) take=1+dp[i][amt-coins[i]];
+                dp[i][amt]=min(notTake,take);
+            }
+        }
+
+        int res=dp[coins.size()-1][amount];
         return res<1e9?res:-1;
     }
 };
