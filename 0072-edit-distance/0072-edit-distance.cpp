080a9e2de39254aaece1eb560cf8c5a9e333b4f3
Rithik Gundeti <rithikgundeti@gmail.com>
2024-06-13 01:46:00 +0530

    Time: 9 ms (48.16%), Space: 12.1 MB (34.39%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -16,8 +16,28 @@ public:
         }
     }
     
-    int minDistance(string word1, string word2) {
+    int minDistance(string word1, string word2){
         vector<vector<int>> dp(word1.length()+1,vector<int>(word2.length()+1,-1));
         return helper(word1,word2,word1.length(),word2.length(),dp)-1;
+        for(int j=0;j<word2.length();j++){
+            dp[0][j]=j+1;
+        }
+        for(int i=0;i<word1.length();i++){
+            dp[i][0]=i+1;
+        }
+        for(int i=1;i<word1.length();i++){
+            for(int j=1;j<word2.length();j++){
+                if(word1[i-1]==word2[j-1]) dp[i][j]=dp[i-1][j-1];
+                else{
+                    int delCase =  1 + dp[i-1][j];
+                    int insertCase =  1 + dp[i][j-1];
+                    int replaceCase =  1 + dp[i-1][j-1];
+
+                    dp[i][j]=min(delCase,min(insertCase,replaceCase));
+                }
+            }
+        }
+
+        return dp[word1.length()][word2.length()];
     }
 };
