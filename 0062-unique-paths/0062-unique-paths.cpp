29fdbccf4f0b166eb5c23cfafbcfb525a3a04d10
Rithik Gundeti <rithikgundeti@gmail.com>
2024-07-26 19:26:51 +0530

    Time: 3 ms (34.01%), Space: 8 MB (6.22%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -15,6 +15,24 @@ public:
     }
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        return helper(m-1,n-1,dp);
+        //return helper(m-1,n-1,dp);
+
+        // base case
+        for(int i=0;i<m;i++){
+            dp[i][0]=1;
+        }
+        for(int j=0;j<n;j++){
+            dp[0][j]=1;
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                int top=dp[i-1][j];
+                int left=dp[i][j-1];
+                dp[i][j]=top+left;
+            }
+        }
+
+        return dp[m-1][n-1];
     }
 };
