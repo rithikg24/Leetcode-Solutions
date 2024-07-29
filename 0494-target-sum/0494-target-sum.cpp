b2d3417dc73efc6db2116d2305f7bc40d0d557d7
Rithik Gundeti <rithikgundeti@gmail.com>
2024-07-29 18:39:17 +0530

    Time: 599 ms (20.31%), Space: 221.6 MB (5.01%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,43 +1,24 @@
 class Solution {
 public:
-    
-    int tabu2FindTarget(vector<int>&nums,int& target)
-    {
-            int sum = 0;
-            for (int num : nums) {
-             sum += num;
+    int helper(vector<int> nums,int i,int target,int res,unordered_map<int,unordered_map<int,int>> &hm){
+        // base
+        if(i==nums.size()){
+            if(target==res){
+                return hm[i][target]=1;
             }
-            if (sum < target || -sum > target) {
-                 return 0;
-                }
-            vector<int> dp(2 * sum + 1, 0);
-            dp[sum] = 1;
-            for (int num : nums) {
-                vector<int> next(2 * sum + 1, 0);
-                for (int i = 0; i < dp.size(); i++) {
-                    if (dp[i] != 0) {
-                    next[i + num] += dp[i];
-                     next[i - num] += dp[i];
-                    }
-            }
-        
-             dp = next;
+            return hm[i][target]=0;
         }
-            return dp[sum + target];
+
+        if(hm.count(i)==1 && hm[i].count(target)==1) return hm[i][target];
+
+        // recursion
+        int add=helper(nums,i+1,target+nums[i],res,hm);
+        int sub = helper(nums,i+1,target-nums[i],res,hm);
+
+        return hm[i][target]=add+sub;
     }
-
     int findTargetSumWays(vector<int>& nums, int target) {
-        
-        int i=0,sum=0,count=0;
-        // solveTarget(nums,target,i,sum,count);
-        
-        vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1,-1));
-        // memoSolveTargetSum(nums,target,i,sum,count,dp);
-        
-        // cout<<dp[0][sum]<<endl;
-
-        int ans2 = tabu2FindTarget(nums,target);// Tabulation 2
-        return ans2 ;
-
+        unordered_map<int,unordered_map<int,int>> hm;
+        return helper(nums,0,0,target,hm);
     }
 };
